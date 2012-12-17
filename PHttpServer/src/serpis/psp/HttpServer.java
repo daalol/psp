921613967bce106a5a85edf91f5ed2fe3fc27478
daalol <daalol87@gmail.com>
2012-12-17 13:00:21 +0100

    Modificacion PHttpServer

--- PHttpServer/src/serpis/psp/HttpServer.java
+++ PHttpServer/src/serpis/psp/HttpServer.java
@@ -1,6 +1,9 @@
 package serpis.psp;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -14,26 +17,55 @@ public class HttpServer {
 	 */
 	public static void main(String[] args) throws IOException {
 		int port=8080;
+		final String fileError404 ="fileError404.html";
+		final String response200="HTTP/1.0 200 OK";
+		final String response404="HTTP/1.0 404 NOT FOUND";
+		final String newLine="\r\n";
 		
 		ServerSocket serverSocket = new ServerSocket(port);
-		
-		
 		Socket socket = serverSocket.accept();
 		Scanner scanner = new Scanner ( socket.getInputStream() );
 		
+		String fileName="index.html";
+		
 		while (true){
 		String line = scanner.nextLine();
+	
 		System.out.println(line);
 		if(line.equals("")) break;
 		}
 		
-		//System.setProperty("line separator", "\r\n");
-		PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+		File file = new File(fileName);
 		
-		printWriter.print("HTTP/1.0 404 Not Found\r\n");
-		printWriter.print("\r\n");
-		printWriter.flush();
+		String responseFileName= file.exists() ? fileName : fileError404;
+		String response= file.exists() ? response200 : response404;
 		
+
+		String header = response+ newLine + newLine;
+		byte[] headerBuffer = header.getBytes();
+		
+		FileInputStream fileInputStream= new FileInputStream(fileName);
+		
+		OutputStream outputStream = socket.getOutputStream();
+		outputStream.write(headerBuffer);
+		
+		final int bufferSize= 2048;
+		byte[] buffer = new byte[bufferSize];
+		
+		int count;
+		while((count = fileInputStream.read(buffer)) != -1){
+			outputStream.write(buffer,0 , count);
+		}
+		
+		
+		fileInputStream.close();
+		
+		/*PrintWriter printWriter = new PrintWriter(socket.getOutputStream(),true);
+		
+		printWriter.print(response + newLine);
+		printWriter.print(newLine);
+		
+		printWriter.close();*/
 		socket.close();
 		
 		serverSocket.close();
